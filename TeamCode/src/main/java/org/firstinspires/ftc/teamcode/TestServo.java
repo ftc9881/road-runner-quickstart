6f41e5c985beb6ffcb752df969cdaf7090ed2604
9881 Team <33109352+ftc9881@users.noreply.github.com>
2024-10-27 19:48:14 -0700

    Update TestServo.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestServo.java
@@ -3,38 +3,43 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="TestServo", group="Robot")
 //@Disabled
 public class TestServo extends LinearOpMode {
 
-        public CRServo intake = null;
+        public Servo servo = null;
 
         /*
          * Code to run ONCE when the driver hits INIT
          */
         @Override
         public void runOpMode() throws InterruptedException {
-             intake = hardwareMap.get(CRServo.class, "intake");
+            servo = hardwareMap.get(Servo.class, "backclaw");
 
             waitForStart();
 
+            double position = 0;
+
+            boolean lastDPadDown = false;
+            boolean lastDPadUp = false;
+
             while (opModeIsActive()) {
-
-                if(gamepad1.x) {
-                    intake.setPower(1);
-                }
-                if(gamepad1.y) {
-                    intake.setPower(.5);
-                }
-                if(gamepad1.a) {
-                    intake.setPower(.25);
-                }
-                if(gamepad1.b) {
-                    intake.setPower(.1);
+                if (gamepad1.dpad_down && !lastDPadDown && position > 0) {
+                    position = position - .05;
                 }
 
-                telemetry.addData("power: ", intake.getPower());
+                if (gamepad1.dpad_up && !lastDPadUp && position < 2) {
+                    position = position + .05;
+                }
+
+                lastDPadDown = gamepad1.dpad_down;
+                lastDPadUp = gamepad1.dpad_up;
+
+                servo.setPosition(position);
+
+                telemetry.addData("position: ", position);
                 telemetry.update();
             }
         }
