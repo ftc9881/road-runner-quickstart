6adc2f2e1a3a8dc4e548b9ea5cec429be422ca86
9881 Team <33109352+ftc9881@users.noreply.github.com>
2024-10-27 19:47:37 -0700

    Create TestCRServo.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCRServo.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+@TeleOp(name="TestCRServo", group="Robot")
+//@Disabled
+public class TestCRServo extends LinearOpMode {
+
+        public CRServo intake = null;
+
+        /*
+         * Code to run ONCE when the driver hits INIT
+         */
+        @Override
+        public void runOpMode() throws InterruptedException {
+             intake = hardwareMap.get(CRServo.class, "intake");
+
+            waitForStart();
+
+            while (opModeIsActive()) {
+
+                if(gamepad1.x) {
+                    intake.setPower(1);
+                }
+                if(gamepad1.y) {
+                    intake.setPower(.5);
+                }
+                if(gamepad1.a) {
+                    intake.setPower(.25);
+                }
+                if(gamepad1.b) {
+                    intake.setPower(.1);
+                }
+
+                telemetry.addData("power: ", intake.getPower());
+                telemetry.update();
+            }
+        }
+    }
